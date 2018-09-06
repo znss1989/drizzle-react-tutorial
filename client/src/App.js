2417610f63fea0386453f9109adaec244ee910bf
Wu Lei <znss1989@gmail.com>
2018-09-06 11:34:17 +0800

    set up drizzle store and drizzle

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import { Drizzle, generateStore } from 'drizzle';
+
 import logo from './logo.svg';
 import './App.css';
+import MyStringStore from './contracts/MyStringStore.json';
+
+const options = {
+  contracts: [MyStringStore]
+};
+
+// setup the drizzle store and drizzle
+const drizzleStore = generateStore(options);
+const drizzle = new Drizzle(options, drizzleStore);
 
 class App extends Component {
   render() {
