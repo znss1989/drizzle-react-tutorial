5cad7d25542f67eb59cf3ab046bea7168a8f642a
Wu Lei <znss1989@gmail.com>
2018-09-06 13:42:10 +0800

    subscribe to drizzle store changes to update state

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    loading: true,
+    drizzleState: null
+  };
+
   render() {
     return (
       <div className="App">
@@ -15,6 +20,19 @@ class App extends Component {
         </p>
       </div>
     );
+  }
+
+  componentDidMount() {
+    const { drizzle } = this.props;
+    // subscribe to changes in the store
+    this.unsubscribe = drizzle.store.subscribe(() => {
+      // every time the store updates, grab the state from drizzle
+      const drizzleState = drizzle.store.getState();
+      // check to see if it's ready, if so, update local component state
+      if (drizzleState.drizzleStatus.initialized) {
+        this.setState({ loading: false, drizzleState });
+      }
+    });
   }
 }
 
