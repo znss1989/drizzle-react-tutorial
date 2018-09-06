cdc222b741289a7044203b5937b3d272bca12b69
Wu Lei <znss1989@gmail.com>
2018-09-06 11:53:31 +0800

    fix error, setup drizzle and pass to app

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Drizzle, generateStore } from 'drizzle';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import MyStringStore from './contracts/MyStringStore.json';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const options = {
+  contracts: [
+    MyStringStore
+  ]
+};
+
+// setup the drizzle store and drizzle
+const drizzleStore = generateStore(options);
+const drizzle = new Drizzle(options, drizzleStore);
+
+ReactDOM.render(<App drizzle={drizzle} />, document.getElementById('root'));
 registerServiceWorker();
